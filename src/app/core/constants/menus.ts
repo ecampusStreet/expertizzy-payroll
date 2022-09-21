cfb85123e95afb8165b8f8ea78f1d3dd92f8cb03
gopalp1 <106673167+gopalp1@users.noreply.github.com>
2022-09-21 10:05:04 +0530

    login page

--- src/app/core/constants/menus.ts
+++ src/app/core/constants/menus.ts
@@ -1,21 +1,9 @@
 export const adminMenu = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/products', title: 'Products',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-        // { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-        // { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/organization', title: 'Organization',  icon:'add_circle', class: '' },
-    { path: '/users', title: 'User',  icon:'person_add', class: '' },
-    { path: '/playstations', title: 'Play Stations',  icon:'sports_tennis', class: '' },
 ];
 
 export const userMenu = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/products', title: 'Products',  icon:'content_paste', class: '' },
-    // { path: '/organization', title: 'Organization',  icon:'add_circle', class: '' },
-    { path: '/users', title: 'User',  icon:'person_add', class: '' },
-    { path: '/playstations', title: 'Play Stations',  icon:'sports_tennis', class: '' },
+    
 ];
---
+++ src/app/menu.ts
@@ -0,0 +1,7 @@
+// export const adminMenu = [
+//     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' }
+// ];
+
+// export const userMenu = [
+//     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' }
+// ];
