5bb629079f4d09fa3107330d4f17deb4dec6c3e8
Vishwanath <vishwab@nfcsolutionsusa.com>
2022-10-06 23:28:20 +0530

    merging experty-g from development

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,35 +1,38 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminRoutingModule } from './admin.routing.module';
 import { AdminComponent } from './admin.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { CoreModule } from '../core/core.module';
 import { LayoutModule } from '../layout/layout.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatRippleModule } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { EmployeesComponent } from './employees/employees.component';
 import { MatIconModule } from '@angular/material/icon';
 import { DatalistComponent } from './datalist/datalist.component';
-import { EmployeesListComponent } from './employees-list/employees-list.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { SharedModule } from '../shared/shared.module';
 import { DashboardCardComponent } from '../core/components';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {MatGridListModule} from '@angular/material/grid-list';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { AdminRoutingModule } from './admin.routing.module';
 @NgModule({
   declarations: [EmployeesComponent, AdminComponent, DatalistComponent,EmployeesListComponent,DashboardComponent,DashboardCardComponent],
   schemas: [NO_ERRORS_SCHEMA],
   imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     AdminRoutingModule,
     CoreModule,
     LayoutModule,
@@ -39,6 +42,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
     SharedModule,
     MatSidenavModule,
     MatCheckboxModule,
+    MatSidenavModule,
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
