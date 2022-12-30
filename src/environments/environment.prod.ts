58deaae2ec12a9e2943216b4b2e3e09dabaf01ef
gopal <gopipattar14@gmail.com>
2022-12-30 15:10:44 +0530

    build config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
+// const apiBaseUrl = "http://localhost:8000/"
+ const apiBaseUrl = "http://swamivivekanandaschool.co.in:8000/"
+const appBase ='';
 export const environment = {
-  production: true
+  apiBaseUrl: apiBaseUrl,
+  production: true,
+  appBase:appBase
 };
